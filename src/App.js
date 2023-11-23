0d9203f1af6589439c6998201086670770e6ca91
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2023-11-22 19:41:25 -0500

    fetch music data with useEffect

--- src/App.js
+++ src/App.js
@@ -7,7 +7,32 @@ import Gallery from "./Components/Gallery/Gallery";
 function App() {
   const [search, setSearch] = useState('')
   const [message, setMessage] = useState('Search for Music')
-  const [data, setData] = useState ([])
+  const [data, setData] = useState([])
+  //https://itunes.apple.com/search?term=black%20sabbath
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://itunes.apple.com/search?term=linkin%20park'
+      const response = await fetch(url)
+      const data = await response.json()
+
+      if (data.results) {
+        setData(data.results)
+      } else {
+        //if you have no results, it resets 
+        setData([])
+        setMessage('Not Found')
+      }
+      
+
+      console.log(data)
+
+    }
+
+    fetchData()
+    //checking stateful variable
+  }, [search])
+
   return (
     <div>
       <SearchBar />
